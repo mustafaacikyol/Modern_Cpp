270b2afd881ccff30247d395dd46105f824e5bc9
Mustafa Açıkyol <95076715+mustafaacikyol@users.noreply.github.com>
2022-06-14 23:40:43 +0300

    Create main.cpp

---
+++ build systems/hello world/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>                                                     // System include files
+
+int main(){
+  
+  std::cout << "Hello World" << << std::endl;                           // This code prints hello world to the screen
+  
+  return 0;
+  
+}
