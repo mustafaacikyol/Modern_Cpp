38a2f261eab6e1928cd3d863be0d379e9ba0e5e7
Mustafa Açıkyol <95076715+mustafaacikyol@users.noreply.github.com>
2022-06-15 12:56:24 +0300

    Create main.cpp

---
+++ STL library/map/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <map>
+#include <string>
+
+int main() {
+
+  std::map<int, std::string> students{{1509, "John"}};
+
+  students.emplace(1000, "Claire");
+  students.emplace(500, "Steven");
+  students.emplace(1700, "Peter");
+
+  for (const auto &[id, name] : students) {
+
+    std::cout << "id : " << id << " " << name << std::endl;
+  }
+
+  std::cout << "Size : " << students.size() << std::endl;
+
+  return 0;
+}
