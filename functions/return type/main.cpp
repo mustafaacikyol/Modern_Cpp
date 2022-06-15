1fcb8fdd3d5dd4c5290e30d413682775a3421269
Mustafa Açıkyol <95076715+mustafaacikyol@users.noreply.github.com>
2022-06-15 12:22:33 +0300

    Create main.cpp

---
+++ functions/return type/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <tuple>
+
+auto Foo(){
+
+    return std::make_tuple("Super Variable", 5);
+
+}
+
+int main(){
+
+    auto[name, value] = Foo();
+    std::cout << name << " has value : " << value << std::endl;
+    return 0;
+    
+}
