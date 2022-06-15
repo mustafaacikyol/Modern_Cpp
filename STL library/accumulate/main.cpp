c35ac0088c63faf7e928a013e241a45091d7467e
Mustafa Açıkyol <95076715+mustafaacikyol@users.noreply.github.com>
2022-06-15 12:35:50 +0300

    Create main.cpp

---
+++ STL library/accumulate/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+
+
+int main(){
+
+    std::vector <int> v{1,2,3,4,5,6,7,8,9,10};
+
+    int sum = std::accumulate(v.begin(),v.end(),0);
+    int product = std::accumulate(v.begin(), v.end(), 1, std::multiplies<int>());
+
+    std::cout << "Sum : " << sum << std::endl;
+    std::cout << "Product : " << product << std::endl;
+
+    return 0;
+
+}
