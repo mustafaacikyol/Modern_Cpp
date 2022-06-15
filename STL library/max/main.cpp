607b4cd5a1bc51612725d3d47ee2e75306ef87e5
Mustafa Açıkyol <95076715+mustafaacikyol@users.noreply.github.com>
2022-06-15 12:57:36 +0300

    Create main.cpp

---
+++ STL library/max/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <iostream>
+
+int main(){
+
+    std::cout << "max(1,9999) : " << std::max(1,9999) << std::endl;
+    std::cout << "max('a','b') : " << std::max('a','b') << std::endl;
+
+    return 0;
+
+}
