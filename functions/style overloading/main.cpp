cd8ba1e4436ba2d24674010aec68810dfdc7b5a3
Mustafa Açıkyol <95076715+mustafaacikyol@users.noreply.github.com>
2022-06-15 12:23:39 +0300

    Create main.cpp

---
+++ functions/style overloading/main.cpp
@@ -0,0 +1,17 @@
+#include<cmath>
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    double x_double = 0.0 ;
+    float x_float = 0.0 ;
+    long double x_long_double = 0.0 ;
+
+    cout << "cos(0) = " << std::cos(x_double) << '\n' ;
+    cout << "cos(0) = " << std::cos(x_float) << '\n';
+    cout << "cos(0) = " << std::cos(x_long_double) << '\n';
+
+    return 0 ;
+    
+}
