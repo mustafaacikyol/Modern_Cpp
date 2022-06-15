612878b94b86c5eaa7480057fa202368acd73fef
Mustafa Açıkyol <95076715+mustafaacikyol@users.noreply.github.com>
2022-06-15 12:15:04 +0300

    Create main.cpp

---
+++ functions/avoid using namespace name/main.cpp
@@ -0,0 +1,28 @@
+#include <cmath>
+#include <iostream>
+using namespace std;
+
+double pow( float x, float exp ){
+
+    float res = 1.0 ;
+
+    for ( int i=0 ; i<exp ; i++ ){
+
+        res *= x;
+
+    }
+
+    return res ;
+
+}
+
+int main(){
+
+    float x = 2 ;
+    float exp = 2 ;
+    
+    cout << "2.0^2 = " << pow( x, exp ) << endl ;
+
+    return 0;
+    
+}
