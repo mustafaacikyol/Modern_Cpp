1ffb2936ed9b62477052e67e6084c25b97700d7f
Mustafa Açıkyol <95076715+mustafaacikyol@users.noreply.github.com>
2022-06-20 19:44:09 +0300

    Create main.cpp

---
+++ utilities/filename part 2/main.cpp
@@ -0,0 +1,15 @@
+#include <filesystem>
+#include <iostream>
+namespace fs = std::filesystem;
+
+int main(){
+
+    std::cout << fs::path("/foo/.bar").filename() << '\n'
+              << fs::path(".").filename() << '\n'
+              << fs::path("..").filename() << '\n'
+              << fs::path("/").filename() << '\n'
+              << fs::path("//host").filename() << '\n';
+
+    return 0;
+
+}
