8f707b4aca89d4b95eaa9b14283196439e088e0f
Mustafa Açıkyol <95076715+mustafaacikyol@users.noreply.github.com>
2022-06-20 19:48:35 +0300

    Create main.cpp

---
+++ utilities/ofstream/main.cpp
@@ -0,0 +1,20 @@
+#include <iomanip>
+#include <fstream>
+using namespace std;
+
+int main(){
+
+    string filename = "out.txt";
+    ofstream outfile(filename);
+    if (!outfile.is_open())
+    {
+        return EXIT_FAILURE;
+    }
+    
+    double a = 1.123123123;
+    outfile << "Just string" << endl;
+    outfile << setprecision(20) << a << endl;
+
+    return 0;
+
+}
