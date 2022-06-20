2ebab599bd0a0d250bd4801a1d85064ab18ba1db
Mustafa Açıkyol <95076715+mustafaacikyol@users.noreply.github.com>
2022-06-20 19:39:27 +0300

    Create main.cpp

---
+++ utilities/extension part 1/main.cpp
@@ -0,0 +1,14 @@
+#include <filesystem>
+#include <iostream>
+namespace fs = std::filesystem;
+
+int main(){
+
+    std::cout << fs::path("/foo/bar.txt").extension() << '\n'
+              << fs::path("/foo/bar.").extension() << '\n'
+              << fs::path("/foo/bar").extension() << '\n'
+              << fs::path("/foo/bar.png").extension() << '\n';
+
+    return 0;
+
+}
